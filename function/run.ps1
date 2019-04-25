6cd65e530803728a665558e8e7b415634fdb72f9
Angel Garcia <angel.g-santos@outlook.com>
2019-04-25 18:31:06 +0200

    Final? version of the ps1 files and resources

--- function/run.ps1
+++ function/run.ps1
@@ -2,20 +2,35 @@
 
 # Get the input request
 $in = Get-Content $req -Raw | ConvertFrom-Json
-$json = $in.RequestBody | ConvertFrom-Json
-
-if ($json.data.monitorCondition -eq "Resolved") {
-   Write-output "Status is " $json.data.monitorCondition
-}
-else {
-	$regexExpression = '\/(?:subscriptions)\/(?<SUBSCRIPTION>[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})\/(?:resourcegroups)\/(?<RESOURCEGROUP>[a-z\-0-9\.]*)\/(?:providers\/microsoft.web\/sites)\/(?<APPSERVICE>[a-z\-0-9\.]*)'
-	$alertPattern = [Regex]::new($regexExpression)
-
-	$matches = $alertPattern.Matches($json.data.essentials.alertTargetIDs)
+Write-Output "IN"
+Write-Output $in
+if ([bool]($in.PSobject.Properties.name -match "RequestBody")) {
+    Write-Host 'From Native Alerts'
+    $json = $in.RequestBody | ConvertFrom-Json
+    $status = $json.data.essentials.monitorCondition
+    
+    $regexExpression = '\/(?:subscriptions)\/(?<SUBSCRIPTION>[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})\/(?:resourcegroups)\/(?<RESOURCEGROUP>[a-z\-0-9\.]*)\/(?:providers\/microsoft.web\/sites)\/(?<APPSERVICE>[a-z\-0-9\.]*)'
+    $alertPattern = [Regex]::new($regexExpression)
+    $matches = $alertPattern.Matches($json.data.essentials.alertTargetIDs)
 
     $resourceGroupName = $matches.Groups[2]
     $appService = $matches.Groups[3]
+}
+else {
+    Write-Host 'From AppInsights'
+    $json = $in
+    $status = $json.status
+    $status = $json.status
+    $resourceGroupName = $json.context.resourceGroupName
+    $appService = $json.context.resourceName
+}
 
+Write-output "Status is $status"
+if ($status -eq "Resolved") {
+    Exit 0
+}
+
+if ($resourceGroupName -ne "") {
     # Application ID for our Azure Security Principal that we created and provided via Function Application Settings
     $username = $env:AzureAutomationAppID
 
@@ -23,7 +38,7 @@ else {
     $pw = $env:AzureAutomationPWD
     $key = Get-Content 'D:\home\site\wwwroot\swords-reboot\gabc-alerts-2019.key'
     $password = $pw | ConvertTo-SecureString -key $key
-    $credentials = New-Object System.Management.Automation.PSCredential $username,$password
+    $credentials = New-Object System.Management.Automation.PSCredential $username, $password
     $AzureRMAccount = Add-AzureRmAccount -Credential $credentials -ServicePrincipal -TenantId $env:AzureAutomationTenantID
     If ($AzureRMAccount) { 
         Restart-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $appService
--- function/runbook.ps1
+++ function/runbook.ps1
@@ -2,28 +2,45 @@
 
 [OutputType("PSAzureOperationResponse")]
 param ( 
-	[Parameter (Mandatory=$false)]
+    [Parameter (Mandatory = $false)]
     [object] $WebhookData
 )
 
 $ErrorActionPreference = "stop"
+if ($WebhookData) {
+    $json = $WebhookData.RequestBody | ConvertFrom-Json
+    $regexExpression = '\/(?:subscriptions)\/(?<SUBSCRIPTION>[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})\/(?:resourcegroups)\/(?<RESOURCEGROUP>[a-z\-0-9\.]*)\/(?:providers\/microsoft.web\/sites)\/(?<APPSERVICE>[a-z\-0-9\.]*)'
+    $alertPattern = [Regex]::new($regexExpression)
+    $matches = $alertPattern.Matches($json.data.essentials.alertTargetIDs)
 
-if ($WebhookData)
-{
-	$json = $WebhookData.RequestBody | ConvertFrom-Json
-	
-	$regexExpression = '\/(?:subscriptions)\/(?<SUBSCRIPTION>[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})\/(?:resourcegroups)\/(?<RESOURCEGROUP>[a-z\-0-9\.]*)\/(?:providers\/microsoft.web\/sites)\/(?<APPSERVICE>[a-z\-0-9\.]*)'
-	$alertPattern = [Regex]::new($regexExpression)
-	$matches = $alertPattern.Matches($json.data.essentials.alertTargetIDs)
-
+    $subscription = $matches.Groups[1]
     $resourceGroupName = $matches.Groups[2]
-	$appService = $matches.Groups[3]
+    $appService = $matches.Groups[3]
 	
-	if ($WebhookData.data.essentials.monitorCondition -eq "Resolved") {	
-		Write-output "Status is " $WebhookData.data.essentials.monitorCondition
-	}
-	else {
-		Write-Output $resourceGroupName
-		Write-Output $appService	
-	}	
+    Write-output "Status is " $WebhookData.data.essentials.monitorCondition
+    if ($WebhookData.data.essentials.monitorCondition -eq "Resolved") {	
+        Exit 0
+    }
+    else {
+        Write-Output $resourceGroupName
+        Write-Output $appService
+		
+        if ($resourceGroupName -ne "") {
+            # Authenticate to Azure by using the service principal and certificate. Then, set the subscription.
+            Write-Verbose "Authenticating to Azure with service principal and certificate" -Verbose
+            $ConnectionAssetName = "AzureRunAsConnection"
+            Write-Verbose "Get connection asset: $ConnectionAssetName" -Verbose
+            $Conn = Get-AutomationConnection -Name $ConnectionAssetName
+            if ($null -eq $Conn) {
+                throw "Could not retrieve connection asset: $ConnectionAssetName. Check that this asset exists in the Automation account."
+            }
+			
+            Write-Verbose "Authenticating to Azure with service principal." -Verbose
+            Add-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Write-Verbose
+            Write-Verbose "Setting subscription to work against: $subscription" -Verbose
+            Set-AzureRmContext -SubscriptionId $subscription -ErrorAction Stop | Write-Verbose
+            Restart-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $appService
+            write-output "==== WebApp restarted ====" $appService
+        }
+    }	
 }
